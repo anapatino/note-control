997f97b76c73d6c409283a0a834e8e08617a311d
Ana Patiño <asofiapatino@unicesar.edu.co>
2022-09-24 13:15:22 -0500

    Login header done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:crud/ui/app.dart';
+import 'package:crud/ui/content/Login/Login.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const Login());
 }
